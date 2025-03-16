10a68cfe260099386d23957d4e471628e3a3c020
Snow Pierce <snowysnowypierce@gmail.com>
2025-03-16 09:52:32 -0400

    v0.0 Alpha initial commit

---
+++ scripts/player_movement.gd
@@ -0,0 +1,24 @@
+extends CharacterBody3D
+
+
+@export var SPEED = 3.0
+@export var JUMP_VELOCITY = 4.5
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
